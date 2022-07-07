7e453f6558c582f057782db2d4a2f4e3f0f8a7e7
aliomurov <33663295+aliomurov@users.noreply.github.com>
2022-07-07 16:29:30 +0600

    Create AppComponent.vue

---
+++ files/resources/js/components/AppComponent.vue
@@ -0,0 +1,13 @@
+<template>
+    <vue-page-transition name="moveInUp" mode="out-in">
+        <router-view></router-view>
+    </vue-page-transition>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
