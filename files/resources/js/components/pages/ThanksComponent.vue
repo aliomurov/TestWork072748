caf99ee4d2acda5057bcbf018b21e9c18b8c692f
aliomurov <33663295+aliomurov@users.noreply.github.com>
2022-07-07 16:31:21 +0600

    Create ThanksComponent.vue

---
+++ files/resources/js/components/pages/ThanksComponent.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <div class="container-fluid">
+            <MenuComponent></MenuComponent>
+            <br>
+            <div class="container">
+                <div class="content">
+                    <div class="row justify-content-between">
+                        <div class="col-sm-6 text-left">
+                            <h2 class="h1">Спасибо за заявку!</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import MenuComponent from "../include/MenuComponent";
+    export default {
+        components:{
+            'MenuComponent': MenuComponent
+        },
+    }
+</script>
+
+<style scoped></style>
