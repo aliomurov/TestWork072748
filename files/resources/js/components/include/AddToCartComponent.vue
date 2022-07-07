3bd737deafb30f50f347d968d19c00fe6bb8fb31
aliomurov <33663295+aliomurov@users.noreply.github.com>
2022-07-07 16:29:33 +0600

    Create AddToCartComponent.vue

---
+++ files/resources/js/components/include/AddToCartComponent.vue
@@ -0,0 +1,42 @@
+<template>
+    <span>
+        <button type="button" class="btn btn-primary btn-sm" @click="addToCart" :disabled="this.stock === 0">
+            <v-icon small class="mr-2">
+                mdi-cart
+            </v-icon>
+        </button>
+    </span>
+</template>
+
+<script>
+    export default {
+        name: "add-to-cart",
+        props: {
+            name: String,
+            price: String,
+            pId: Number,
+            unit: Number,
+            stock: Number,
+        },
+        data() {
+            return {
+                item: {
+                    productName: this.name,
+                    productPrice: this.price,
+                    productId: this.pId,
+                    productUnit: this.unit,
+                    productStock: this.stock,
+                    productQuantity: 1,
+                }
+            }
+        },
+
+        methods: {
+            addToCart(){
+                this.$store.commit('addToCart', this.item)
+            },
+        }
+    }
+</script>
+
+<style scoped></style>
