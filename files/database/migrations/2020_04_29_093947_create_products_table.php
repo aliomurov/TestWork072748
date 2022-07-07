17bf86837bd0eda49fc1403b956b2e89b2306cb8
aliomurov <33663295+aliomurov@users.noreply.github.com>
2022-07-07 16:28:13 +0600

    Create 2020_04_29_093947_create_products_table.php

---
+++ files/database/migrations/2020_04_29_093947_create_products_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('category_id')->unsigned()->nullable();
+            $table->string('name')->nullable();
+            $table->string('slug')->nullable();
+            $table->string('image')->nullable();
+            $table->string('price')->nullable();
+            $table->integer('stock')->nullable();
+            $table->boolean('unit')->default(false);
+
+            $table->timestamps();
+
+            $table->foreign('category_id')
+                ->references('id')
+                ->on('categories')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
